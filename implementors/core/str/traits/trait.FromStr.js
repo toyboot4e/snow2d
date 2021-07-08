4503b4f9add9e76c236390d57b35e2f8086844fb
toyboot4e <toyboot4e@gmail.com>
2021-07-09 00:54:03 +0900

    update

---
+++ implementors/core/str/traits/trait.FromStr.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["snow2d"] = [{"text":"impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/str/traits/trait.FromStr.html\" title=\"trait core::str::traits::FromStr\">FromStr</a> for <a class=\"struct\" href=\"snow2d/asset/struct.AssetKey.html\" title=\"struct snow2d::asset::AssetKey\">AssetKey</a>&lt;'a&gt;","synthetic":false,"types":["snow2d::asset::AssetKey"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
