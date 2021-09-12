066f3c053377e9175e653db677a819bb6703d712
toyboot4e <toyboot4e@gmail.com>
2021-09-12 18:34:17 +0900

    update `snow2d` (`igri` separation)

--- src/utils/inspect/crate_impls.rs
+++ src/utils/inspect/crate_impls.rs
@@ -3,14 +3,9 @@
 */
 
 use igri::{imgui, Inspect};
-use imgui::{im_str, Ui};
+use imgui::Ui;
 
-use crate::{
-    asset::Asset,
-    gfx::tex::*,
-    input::Dir8,
-    utils::{arena, pool},
-};
+use crate::{asset::Asset, gfx::tex::*};
 
 // TODO: prefer blanket impl
 impl Inspect for Asset<Texture2dDrop> {
@@ -18,53 +13,3 @@ impl Inspect for Asset<Texture2dDrop> {
         ui.text(format!("Asset at {}", self.path().display()));
     }
 }
-
-// igri::impl_unit_enum! {
-//     Dir8, N, NE, E, SE, S, SW, W, NW
-// };
-//
-// struct D;
-//
-// impl<T: Inspect> Inspect for arena::Arena<T> {
-//     fn inspect(&mut self, ui: &Ui, label: &str) {
-//         imgui::TreeNode::new(&imgui::im_str!("{}", label))
-//             .flags(imgui::TreeNodeFlags::OPEN_ON_ARROW | imgui::TreeNodeFlags::OPEN_ON_DOUBLE_CLICK)
-//             .build(ui, || {
-//                 for (i, item) in self.items_mut().enumerate() {
-//                     item.inspect(ui, im_str!("{}", i).to_str());
-//                 }
-//             });
-//     }
-// }
-//
-// impl<T> Inspect for arena::Index<T> {
-//     fn inspect(&mut self, ui: &Ui, _label: &str) {
-//         ui.text("TODO: Index<T>");
-//     }
-// }
-//
-// impl<T> Inspect for pool::Handle<T> {
-//     fn inspect(&mut self, ui: &Ui, _label: &str) {
-//         ui.text("TODO: Handle<T>");
-//     }
-// }
-//
-// // impl<T> Inspect for pool::WeakHandle<T> {
-// impl<T> Inspect for pool::WeakHandle<T> {
-//     fn inspect(&mut self, ui: &Ui, _label: &str) {
-//         ui.text("TODO: WeakHandle<T>");
-//     }
-// }
-//
-// // impl<T: Inspect + 'static> Inspect for pool::Pool<T> {
-// impl<T: Inspect + 'static> Inspect for pool::Pool<T> {
-//     fn inspect(&mut self, ui: &Ui, label: &str) {
-//         imgui::TreeNode::new(&imgui::im_str!("{}", label))
-//             .flags(imgui::TreeNodeFlags::OPEN_ON_ARROW | imgui::TreeNodeFlags::OPEN_ON_DOUBLE_CLICK)
-//             .build(ui, || {
-//                 for (i, item) in self.iter_mut().enumerate() {
-//                     item.inspect(ui, im_str!("{}", i).to_str());
-//                 }
-//             });
-//     }
-// }
